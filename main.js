4dbb7c446a51ecbf6ad23d1421d04aa5a5333b8e
Ibukun <adejumoibk702@gmail.com>
2023-11-18 01:04:00 +0100

    added font to sub boxes, created the toggle button and the next button on step 2 and also the go back button

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const number2 = document.querySelector(".number2");
 
 //  hidden pages
 
-document.querySelector(".info-display").classList.remove("hidden-Display-step1");
+// document.querySelector(".info-display").classList.remove("hidden-Display-step1");
 const hiddenPage1 = document.querySelector(".info-display");
 const hiddenPage2 = document.querySelector(".info-display2");
 
@@ -19,17 +19,23 @@ const redRequired2 = document.querySelector(".popDisplayNumber");
 const arcadeBtn = document.getElementById("arcadeBtn--");
 const advancedBtn = document.getElementById("advancedBtn--");
 const proBtn = document.getElementById("proBtn--");
+const toggleSub = document.querySelector(".toggle-btn");
+const toggleMonth = document.querySelector(".inner-circle-month");
+const toggleYear = document.querySelector(".inner-circle-year");
+
+
+// making the month toggle visible
+document.querySelector(".inner-circle-month").classList.remove('hidden-month')
+
 // arcadeBtn.value = 9
 // advancedBtn.value = 12
 // proBtn.value = 15
 
-
 // console.log(arcadeBtn.value);
 // console.log(arcadeBtnValue);
 
-
 // // subsciption prices in step 2
-// const arcadePrice = '9' 
+// const arcadePrice = '9'
 
 // // next btn on the first page
 // const arcadeBtn = document.getElementById("btn-arcade");
@@ -38,52 +44,67 @@ const proBtn = document.getElementById("proBtn--");
 // })
 
 //  function savedValue () {
-//   const arcadeBtnValue = 9 
+//   const arcadeBtnValue = 9
 //   // return arcadeBtnValue
 //   console.log(arcadeBtnValue);
 // }
 
 // savedValue()
-const subDisplay = document.querySelector('.sub-type')
+const subDisplay = document.querySelector(".sub-type");
 
-
+// arcade button
 arcadeBtn.addEventListener("click", function () {
-  const arcadeBtnValue = 9 
-  subDisplay.textContent = 'Arcade'
-  arcadeBtn.classList.add('sub-box-on-click')
-  advancedBtn.classList.remove('sub-box-on-click')
-  proBtn.classList.remove('sub-box-on-click')
+  const arcadeBtnValue = 9;
+  subDisplay.textContent = "Arcade";
+  arcadeBtn.classList.add("sub-box-on-click");
+  advancedBtn.classList.remove("sub-box-on-click");
+  proBtn.classList.remove("sub-box-on-click");
 
   // arcadeBtn.classList.remove('.sub-box-on-click')
 
-
-
   console.log(arcadeBtnValue);
-})
+});
 
+// advanced button
 advancedBtn.addEventListener("click", function () {
-  const advancedBtnValue = 12 
-  subDisplay.textContent = 'Advanced'
-  advancedBtn.classList.add('sub-box-on-click')
-  arcadeBtn.classList.remove('sub-box-on-click')
-  proBtn.classList.remove('sub-box-on-click')
+  const advancedBtnValue = 12;
+  subDisplay.textContent = "Advanced";
+  advancedBtn.classList.add("sub-box-on-click");
+  arcadeBtn.classList.remove("sub-box-on-click");
+  proBtn.classList.remove("sub-box-on-click");
 
   console.log(advancedBtnValue);
-})
+});
+
+// pro button
 proBtn.addEventListener("click", function () {
-  const proBtnValue = 15 
-  subDisplay.textContent = 'pro'
-  proBtn.classList.add('sub-box-on-click')
-  arcadeBtn.classList.remove('sub-box-on-click')
-  advancedBtn.classList.remove('sub-box-on-click')
+  const proBtnValue = 15;
+  subDisplay.textContent = "pro";
+  proBtn.classList.add("sub-box-on-click");
+  arcadeBtn.classList.remove("sub-box-on-click");
+  advancedBtn.classList.remove("sub-box-on-click");
 
   console.log(proBtnValue);
-})
+});
+// toggle button
+let activeSub = 0;
+toggleSub.addEventListener("click", function () {
+  if (activeSub === 0) {
+    console.log("switch to next");
+    activeSub = 1;
+    toggleMonth.classList.add('hidden-month')
+    toggleYear.classList.remove('hidden-year')
+  } else if (activeSub === 1) {
+    console.log("back to prev");
+    activeSub = 0;
+    toggleMonth.classList.remove('hidden-month')
+    toggleYear.classList.add('hidden-year')
+  }
+});
 
-
-
-const nextBtn = document.getElementById("btn--1");
-nextBtn.addEventListener("click", function () {
+// next step button for page 1
+const nextBtnStep1 = document.getElementById("btn--1");
+nextBtnStep1.addEventListener("click", function () {
   let outName = inputName.value;
   let outEmail = inputEmail.value;
   let outNumber = inputNumber.value;
@@ -105,13 +126,13 @@ nextBtn.addEventListener("click", function () {
     redRequired.classList.remove("hiddenPopName");
   } else {
     // Code to execute if both condition1 and condition2 are false
-   
+
     console.log("next step");
     number1.style.backgroundColor = "transparent";
     number2.style.backgroundColor = "#a7eeee";
 
     // switch to step 2
-   
+
     hiddenPage1.classList.add("hidden-Display-step1");
     hiddenPage2.classList.remove("hidden-Display-step2");
   }
@@ -120,4 +141,3 @@ nextBtn.addEventListener("click", function () {
 
   //  console.log(inputValue);
 });
-
