d63af18584343d5ecb1d4d6888063bc39a31f87a
Ibukun <adejumoibk702@gmail.com>
2023-11-22 02:16:28 +0100

    added a 3rd step and finished the functionality of step 2

--- main.js
+++ main.js
@@ -2,12 +2,22 @@
 // const number1 =
 const number1 = document.querySelector(".number");
 const number2 = document.querySelector(".number2");
+const number3 = document.querySelector(".number3");
+
 
 //  hidden pages
 
-document.querySelector(".info-display").classList.remove("hidden-Display-step1");
+document.querySelector(".info-display").classList.add("hidden-Display-step1");
+
 const hiddenPage1 = document.querySelector(".info-display");
 const hiddenPage2 = document.querySelector(".info-display2");
+const hiddenPage3 = document.querySelector(".info-display3");
+// bsbsbsbsb
+hiddenPage2.classList.add('hidden-Display-step2')
+hiddenPage3.classList.remove('hidden-Display-step3')
+const blueMonth = document.querySelector(".toggle-details1");
+const blueYear = document.querySelector(".toggle-details2");
+blueYear.classList.remove("yearly-blue");
 
 // input fields in step 1 page
 
@@ -16,59 +26,79 @@ const inputEmail = document.getElementById("email--");
 const inputNumber = document.getElementById("phoneNumber--");
 const redRequired = document.querySelector(".popDisplayName");
 const redRequired2 = document.querySelector(".popDisplayNumber");
+const nextBtnStep1 = document.getElementById("btn--1");
+
+// buttons and elements on step 2
+const redRequired3 = document.querySelector('.required-text')
+const previousStep1 = document.getElementById("btn--2");
+const nextBtnStep2 = document.getElementById("btn--3");
 const arcadeBtn = document.getElementById("arcadeBtn--");
 const advancedBtn = document.getElementById("advancedBtn--");
 const proBtn = document.getElementById("proBtn--");
 // const priceMonth = document.querySelector(".sub-price-details");
-const priceYear = document.querySelectorAll(".sub-price-details-year");
-const priceYearDetails = document.querySelectorAll(".sub-price-details-year-info");
+const priceYearDetails = document.querySelectorAll(
+  ".sub-price-details-year-info"
+);
 
 const toggleSub = document.querySelector(".toggle-btn");
 const toggleMonth = document.querySelector(".inner-circle-month");
 const toggleYear = document.querySelector(".inner-circle-year");
-const priceMonth = document.querySelectorAll(".sub-price-details")
-priceMonth.forEach(element => {
-  element.classList.remove('year-out-click');
-})
+const priceMonthArcade = document.querySelector(".sub-price-details1");
+const priceMonthAdvanced = document.querySelector(".sub-price-details2");
+const priceMonthPro = document.querySelector(".sub-price-details3");
 
-
-
+// priceMonth.forEach(element => {
+//   element.classList.remove('year-out-click');
+// })
 
 // making the month toggle visible
 document.querySelector(".inner-circle-month").classList.remove("hidden-month");
 
-// arcadeBtn.value = 9
-// advancedBtn.value = 12
-// proBtn.value = 15
-
-// console.log(arcadeBtn.value);
-// console.log(arcadeBtnValue);
-
-// // subsciption prices in step 2
-// const arcadePrice = '9'
-
-// // next btn on the first page
-// const arcadeBtn = document.getElementById("btn-arcade");
-// arcadeBtn.addEventListener("click", function () {
-//   console.log(arcadePrice);
-// })
-
-//  function savedValue () {
-//   const arcadeBtnValue = 9
-//   // return arcadeBtnValue
-//   console.log(arcadeBtnValue);
-// }
-
-// savedValue()
 const subDisplay = document.querySelector(".sub-type");
+// display price of year sub click function
+
+// price for month function on step 2
+
+const priceForYear = function () {
+  priceMonthArcade.textContent = "$90/yr";
+  priceMonthAdvanced.textContent = "$120/yr";
+  priceMonthPro.textContent = "$150/yr";
+  if (subDisplay.textContent == "$9/mo") {
+    subDisplay.textContent = priceMonthArcade.textContent;
+    console.log(`9 dolls`);
+  } else if (subDisplay.textContent == "$12/mo") {
+    subDisplay.textContent = priceMonthAdvanced.textContent;
+
+    console.log("12 dolls");
+  } else if (subDisplay.textContent == "$15/mo") {
+    subDisplay.textContent = priceMonthPro.textContent;
+  }
+};
+
+// price for month function on step 2
+const priceForMonth = function () {
+  priceMonthArcade.textContent = "$9/mo";
+  priceMonthAdvanced.textContent = "$12/mo";
+  priceMonthPro.textContent = "$15/mo";
+  if (subDisplay.textContent == "$90/yr") {
+    subDisplay.textContent = priceMonthArcade.textContent;
+    console.log(`9 dolls`);
+  } else if (subDisplay.textContent == "$120/yr") {
+    subDisplay.textContent = priceMonthAdvanced.textContent;
+
+    console.log("12 dolls");
+  } else if (subDisplay.textContent == "$150/yr") {
+    subDisplay.textContent = priceMonthPro.textContent;
+  }
+};
 
 // arcade button
 arcadeBtn.addEventListener("click", function () {
   const arcadeBtnValue = 9;
-  subDisplay.textContent = "Arcade";
   arcadeBtn.classList.add("sub-box-on-click");
   advancedBtn.classList.remove("sub-box-on-click");
   proBtn.classList.remove("sub-box-on-click");
+  subDisplay.textContent = priceMonthArcade.textContent;
 
   // arcadeBtn.classList.remove('.sub-box-on-click')
 
@@ -78,10 +108,11 @@ arcadeBtn.addEventListener("click", function () {
 // advanced button
 advancedBtn.addEventListener("click", function () {
   const advancedBtnValue = 12;
-  subDisplay.textContent = "Advanced";
+
   advancedBtn.classList.add("sub-box-on-click");
   arcadeBtn.classList.remove("sub-box-on-click");
   proBtn.classList.remove("sub-box-on-click");
+  subDisplay.textContent = priceMonthAdvanced.textContent;
 
   console.log(advancedBtnValue);
 });
@@ -89,13 +120,14 @@ advancedBtn.addEventListener("click", function () {
 // pro button
 proBtn.addEventListener("click", function () {
   const proBtnValue = 15;
-  subDisplay.textContent = "pro";
   proBtn.classList.add("sub-box-on-click");
   arcadeBtn.classList.remove("sub-box-on-click");
   advancedBtn.classList.remove("sub-box-on-click");
+  subDisplay.textContent = priceMonthPro.textContent;
 
   console.log(proBtnValue);
 });
+
 // toggle button
 let activeSub = 0;
 toggleSub.addEventListener("click", function () {
@@ -104,38 +136,29 @@ toggleSub.addEventListener("click", function () {
     activeSub = 1;
     toggleMonth.classList.add("hidden-month");
     toggleYear.classList.remove("hidden-year");
-    priceMonth.forEach(element => {
-      element.classList.add('year-out-click');
-    })
+    blueMonth.classList.remove("monthly-blue");
+    blueYear.classList.add("yearly-blue");
 
-    priceYear.forEach(element => {
-      element.classList.remove('year-on-click');
-    })
-    priceYearDetails.forEach(element => {
-      element.classList.remove('year-on-click');
-    })
-   
-   
+    priceYearDetails.forEach((element) => {
+      element.classList.remove("year-on-click");
+    });
+    priceForYear();
   } else if (activeSub === 1) {
     console.log("back to prev");
     activeSub = 0;
     toggleMonth.classList.remove("hidden-month");
     toggleYear.classList.add("hidden-year");
-    priceMonth.forEach(element => {
-      element.classList.remove('year-out-click');
-    })
+    blueMonth.classList.add("monthly-blue");
+    blueYear.classList.remove("yearly-blue");
 
-    priceYear.forEach(element => {
-      element.classList.add('year-on-click');
-    })
-    priceYearDetails.forEach(element => {
-      element.classList.add('year-on-click');
-    })
+    priceYearDetails.forEach((element) => {
+      element.classList.add("year-on-click");
+    });
+    priceForMonth();
   }
 });
 
-// next step button for page 1
-const nextBtnStep1 = document.getElementById("btn--1");
+// next step button for step 1
 nextBtnStep1.addEventListener("click", function () {
   let outName = inputName.value;
   let outEmail = inputEmail.value;
@@ -167,9 +190,44 @@ nextBtnStep1.addEventListener("click", function () {
 
     hiddenPage1.classList.add("hidden-Display-step1");
     hiddenPage2.classList.remove("hidden-Display-step2");
+    // inputName.value = outName.value
   }
 
   console.log("Input Value:", outName, outEmail, outNumber);
 
   //  console.log(inputValue);
+});
+
+// go back  button to step 1
+previousStep1.addEventListener("click", function () {
+  event.preventDefault();
+  hiddenPage1.classList.remove("hidden-Display-step1");
+  hiddenPage2.classList.add("hidden-Display-step2");
+  number1.style.backgroundColor = "#a7eeee";
+  number2.style.backgroundColor = "transparent";
+
+  // switch to step 2
+});
+
+// next step button for step 2
+nextBtnStep2.addEventListener("click", function name(params) {
+  if (
+    subDisplay.textContent == "$90/yr" ||
+    subDisplay.textContent == "$9/mo" ||
+    subDisplay.textContent == "$120/yr" ||
+    subDisplay.textContent == "$12/mo" ||
+    subDisplay.textContent == "$15/mo" ||
+    subDisplay.textContent == "$150/yr"
+  ) {
+
+    hiddenPage2.classList.add('hidden-Display-step2')
+    hiddenPage3.classList.remove('hidden-Display-step3')
+    number1.style.backgroundColor = "transparent";
+    number2.style.backgroundColor = "transparent";
+    number3.style.backgroundColor = "#a7eeee";
+
+    console.log("hmm");
+  } else {
+redRequired3.textContent = 'Please select a plan to proceed'
+  }
 });
